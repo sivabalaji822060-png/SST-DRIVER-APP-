f232f26124906661e243b7a4013a7c0fb8fa5486
sivabalaji822060-png <sivabalaji822060@gmail.com>
2026-09-10 21:30:39 +0530

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,3 +0,0 @@
-const CACHE='sst-driver-v6';const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./splash.jpg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
